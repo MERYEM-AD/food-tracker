c24984ed169716b5947d8c50c0488c3bc5faa69f
bripap <bpappas302@gmail.com>
2021-10-26 19:55:35 -0400

    trying to add API to contact us page

--- Assets/js/contactUs.js
+++ Assets/js/contactUs.js
@@ -1,13 +1,22 @@
 function sendEmail() {
+
+    let userName = document.getElementById("name").value
+    let userEmail = document.getElementById("email").value
+    let userMessage = document.getElementById("message").value
+
+
         Email.send({
-        Host: "smtp.gmail.com",
-        Username: "foodtrackerapplication@gmail.com",
-        Password: "bootcampgroup1",
-        To: "foodtrackerapplication@gmailcom",
+        // Host: "smtp.gmail.com",
+        // Username: "foodtrackerapplication@gmail.com",
+        // Password: "bootcampgroup1", 
+        SecureToken:"bcd89ce0-1dea-4997-ad8d-523f52e30597",
         From: "foodtrackerapplication@gmail.com",
-        Subject: "subject of the email",
-        Body: "body of the email",    
-    }).then(
-        message => alert("mail sent successfully")
-    )
+        To: "foodtrackerapplication2@gmailcom",
+        Name: userName,
+        Email: userEmail,
+        Body: userMessage,    
+    })
+    .then(function(message) {
+        alert("mail sent successfully")
+    });
 }
--- README.md
+++ README.md
@@ -5,8 +5,9 @@ Project 1
 
 ### Project description:  
 
-Web-based application that allows user to track calories on a daily basis. 
+Web-based application that allows user to track calories on a daily basis. Also allows users to research recipies. 
+
 The goal of the application is to help user maintain a certain weight, lose weight, or gain weight depending on their goals. When the user joins the app, they will tell us the weight they want to maintain or achieve. Then they will be able to add the calories of the food that they are consuming daily. The application will then calculate the calories and will notify the user how many calories they need to maintain, lose, or gain weight.
 
 ### User story: 
-As an athlete, I want to be able to track my calories so I can maintain my weight.
+As a user, I want to be able to track my calories and/or food nutrition so I can maintain my weight.
