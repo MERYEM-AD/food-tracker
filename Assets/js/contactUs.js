d6d0c595c92644c204f9844000f804b77e6584a9
bripap <bpappas302@gmail.com>
2021-10-28 18:06:05 -0400

    updating contactUs.js with working sendEmail function.

--- Assets/js/contactUs.js
+++ Assets/js/contactUs.js
@@ -1,13 +1,15 @@
-const contactusForm = document.querySelector('#contactusForm');
+const contactusForm = document.querySelector('#contactUsForm');
 
 contactusForm.addEventListener('submit',function(event){
     event.preventDefault();
+    sendEmail();
+});
 
 function sendEmail() {
 
-    let userName = document.getElementById("name").value
-    let userEmail = document.getElementById("email").value
-    let userMessage = document.getElementById("message").value
+    let userName = document.getElementById("name").value;
+    let userEmail = document.getElementById("email").value;
+    let userMessage = document.getElementById("message").value;
     
 
         Email.send({
@@ -15,15 +17,16 @@ function sendEmail() {
         // Username: "foodtrackerapplication@gmail.com",
         // Password: "bootcampgroup1", 
         SecureToken:"bcd89ce0-1dea-4997-ad8d-523f52e30597",
-        From: "foodtrackerapplication@gmail.com",
-        To: "foodtrackerapplication2@gmailcom",
+        From: userEmail,
+        To: "foodtrackerapplication@gmail.com",
         Name: userName,
         Email: userEmail,
-        Body: userMessage,    
+        Body: userMessage, 
+        Subject: "Test"   
     })
     .then(function(message) {
         alert("mail sent successfully")
+    }).catch(function(error) {
+        console.log(error);
     });
 }
-
-btn.addEventListener("click", sendEmail);
